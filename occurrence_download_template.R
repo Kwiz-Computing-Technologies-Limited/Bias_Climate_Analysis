b106c0f9ab2ac99807ac468345fc98610eb3d5bc
jean_KRS <jeankwizera@kwizresearch.com>
2023-01-17 19:32:52 +0300

    bug fix4

--- occurrence_download_template.R
+++ occurrence_download_template.R
@@ -25,22 +25,9 @@ if(!("name_backbone" %in% list.files(here()))) {
 
 # function to download the relevant columns from dropbox
 get_data = function(dataset, drop_url) {
-  set_a = c("aus_glonaf_invaded_clean", "aus_randal_invaded_clean",
-            "gbr_native_clean", "gbr_invaded_clean", "irl_native_clean", 
-            "irl_invaded_clean", "jpn_invaded_clean", "jpn_native_clean",
-            "mdg_native_clean", "mdg_invaded_clean", "nzl_aiko_invaded_clean",
-            "nzl_aiko_native_clean", "nzl_glonaf_native_clean", "nzl_glonaf_invaded_clean",
-            "usa_native_clean", "usa_invaded_clean", "zaf_native_clean", "zaf_invaded_clean")
-  
   paste("fetching", dataset, "occurrences") |> print()
-  if(dataset %in% set_a) {
-    value = readr::read_csv(file = drop_url) 
-  } else {
-    tryCatch({
-      value = read.csv(file = drop_url)
-    })
-  }
-  
+  value = readr::read_csv(file = drop_url)
+   
   value = value[complete.cases(value$species), ] 
   #paste("Fetch complete! Saving", dataset, "occurrences to file") |> print()
   #readr::write_csv(x = value, file = here("occurrence_files", paste0(dataset, ".csv")))
