c331129ba1bc038d2eb5b95070519b47d3b93be7
jean_KRS <jeankwizera@kwizresearch.com>
2023-01-17 16:53:11 +0300

    bug fix2

--- 13.  bias assessment results/run_bias_assessments.R
+++ 13.  bias assessment results/run_bias_assessments.R
@@ -1,6 +1,6 @@
 
 # source bias function
-source("~/Desktop/Documents/GitHub/bias assessment/occAssess_bias_function.R")
+source(here("occAssess_bias_function.R"))
 
 # urls to be downloaded from Dropbox
 occ_data = tibble(dataset = c("aus_glonaf_invaded_clean",
@@ -15,17 +15,17 @@ occ_data = tibble(dataset = c("aus_glonaf_invaded_clean",
                               "usa_invaded_clean",
                               "zaf_invaded_clean"), 
                   
-                  drop_url = c("https://www.dropbox.com/s/6kkdjtvdh52s0oo/AUS-Randall-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/rg1h1wm1ctje9dh/AUS-Glonaf-invaded-clean-v1.csv?dl=0",
-                               "https://www.dropbox.com/s/rbyvnu3wap6wyh7/GBR-invaded-clean-v1.csv?dl=0",
-                               "https://www.dropbox.com/s/6093mg9vl4ibhrj/IRL-NIR-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/c6y84zikkrvtqjs/JPN-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/m8yxddeze4hovpv/MDG-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/cw2cm97mc7wnjyq/NZL-Aikio-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/rkb7a2rutw7vsel/NZL-Glonaf-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/mtqukig658nd3ov/NZL-Randall-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/7blp6dvql7kf9xk/USA-invaded-clean.csv?dl=0",
-                               "https://www.dropbox.com/s/rn5y3vebqt5igad/ZAF-invaded-clean.csv?dl=0")) |>
+                  drop_url = c("https://www.dropbox.com/s/6kkdjtvdh52s0oo/AUS-Randall-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/rg1h1wm1ctje9dh/AUS-Glonaf-invaded-clean-v1.csv?dl=1",
+                               "https://www.dropbox.com/s/rbyvnu3wap6wyh7/GBR-invaded-clean-v1.csv?dl=1",
+                               "https://www.dropbox.com/s/6093mg9vl4ibhrj/IRL-NIR-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/c6y84zikkrvtqjs/JPN-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/m8yxddeze4hovpv/MDG-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/cw2cm97mc7wnjyq/NZL-Aikio-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/rkb7a2rutw7vsel/NZL-Glonaf-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/mtqukig658nd3ov/NZL-Randall-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/7blp6dvql7kf9xk/USA-invaded-clean.csv?dl=1",
+                               "https://www.dropbox.com/s/rn5y3vebqt5igad/ZAF-invaded-clean.csv?dl=1")) |>
   na.omit()
 
 
