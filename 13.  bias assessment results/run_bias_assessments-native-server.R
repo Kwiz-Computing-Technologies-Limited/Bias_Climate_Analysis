2bfcb9e30a1a98db12ca3f80c9fa63639f53e8cb
jeanKRS <jeankwizera@kwizresearch.com>
2023-05-05 14:07:50 +0200

    latest updates

--- 13.  bias assessment results/run_bias_assessments-native-server.R
+++ 13.  bias assessment results/run_bias_assessments-native-server.R
@@ -3,24 +3,27 @@
 source("C:/Users/Robeck/Downloads/Bias_Climate_Analysis/occAssess_bias_function.R")
 
 # urls to be downloaded from Dropbox
-occ_data = tibble(dataset = c(# "gbr-native-filtered",
+occ_data = tibble(dataset = c( "gbr-native-filtered",
                               # "irl-nir-native-filtered", 
                               # "jpn-native-filtered", 
                               # "mdg-native-filtered",
                               # "zaf-native-filtered", 
                               # "nzl-Glonaf-native-clean",
-                              # "usa-native-filtered",
-                              "aus_glonaf_native_filtered"), 
+                               
+                              #"aus_glonaf_native_filtered",
+                              "usa-native-filtered"
+                              ), 
                   
-                  drop_url = c(# "https://www.dropbox.com/s/jy57axiea62cqr8/GBR-native-filtered.csv?dl=1",
+                  drop_url = c( "https://www.dropbox.com/s/jy57axiea62cqr8/GBR-native-filtered.csv?dl=1",
                                # "https://www.dropbox.com/s/9a9ww83psn12rqw/IRL-NIR-native-filtered.csv?dl=1",
                                # "https://www.dropbox.com/s/vsshp1x06bstwbx/JPN-native-filtered.csv?dl=1",
                                # "https://www.dropbox.com/s/avjerougghb56j0/MDG-native-filtered.csv?dl=1",
                                # "https://www.dropbox.com/s/ojemwko9qctjt1h/ZAF-native-filtered.csv?dl=1",
                                # "https://www.dropbox.com/s/ig0mqx06n31e5rq/NZL-Glonaf-native-clean.csv?dl=1",
-                               # "https://www.dropbox.com/s/ufmvdr7f6t73cbs/USA-native-filtered.csv?dl=1",
-                               "https://www.dropbox.com/s/388ugmdgem36359/AUS-Glonaf-native-filtered-v1.csv?dl=1")) |>
-  na.omit()
+                               
+                               #"https://www.dropbox.com/s/388ugmdgem36359/AUS-Glonaf-native-filtered-v1.csv?dl=1",
+                               "https://www.dropbox.com/s/ufmvdr7f6t73cbs/USA-native-filtered.csv?dl=1"
+                               )) 
 
 
 # generate bias output files
